e5fe3b1b1991440a51380f944ea0d2008f950f7e
axion-labs <164126160+axion-labs@users.noreply.github.com>
2024-05-06 16:32:08 +0530

    devnet

--- utils/openbook.ts
+++ utils/openbook.ts
@@ -13,9 +13,9 @@ import {
 } from "../hooks/useOpenbookClient";
 
 // MAINNET
-export const RPC = "https://misty-wcb8ol-fast-mainnet.helius-rpc.com/";
+//export const RPC = "https://misty-wcb8ol-fast-mainnet.helius-rpc.com/";
 // DEVNET
-// export const RPC = "https://aimil-f4d13p-fast-devnet.helius-rpc.com/";
+export const RPC = "https://aimil-f4d13p-fast-devnet.helius-rpc.com/";
 
 export const fetchData = async () => {
   const connection = useHookConnection();
